921df0809d69b7faeb924d34921a1bda3d1a3b39
duduSC <dudu.camargo104@gmail.com>
2026-04-03 02:56:27 -0300

    correção de nomes de tabela na etl.ipynb, e correção do script para buscar da API as datas e criar a dim_data e fato_data

--- etl.py
+++ etl.py
@@ -4,7 +4,7 @@ import pandas as pd
 from tqdm import tqdm
 import os
 from dotenv import load_dotenv
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, text
 import numpy as np
 
 load_dotenv()
@@ -14,26 +14,29 @@ base_url = "https://api.themoviedb.org/3"
 db = create_engine(f'postgresql://eduar:{DB_SENHA}@localhost:5432/DW_Filmes')
 # Para limitar o número de conexões em 20
 sem = asyncio.Semaphore(20)
-
-
+lista_ids_datas = dict()
+valores_com_erro = []
 def transforma_dim_data(df:pd.DataFrame):
    
-    dim_data = pd.to_datetime(df["data"])
-    dim_data = pd.DataFrame(dim_data)
+    df_limpo = df.copy()
+    df_limpo["data"] = pd.to_datetime(df_limpo["data"], errors='coerce')
+    
+    df_limpo = df_limpo.dropna(subset=['data'])
+    
+    dim_data = pd.DataFrame()
+    dim_data["data_original"] = df_limpo["data"]
 
     dim_data = dim_data.assign(
-        ano= (dim_data["data"].dt.year).astype("str"),
-        mes=(dim_data["data"].dt.month).astype("str"),
-        dia=( dim_data["data"].dt.day).astype("str"),
-        semestre= np.where(dim_data["data"].dt.quarter>2,"1","2"),
-        dia_da_semana= (dim_data["data"].dt.day_of_week).astype("str")
+        ano= (dim_data["data_original"].dt.year).astype(int),
+        mes=(dim_data["data_original"].dt.month).astype(int),
+        dia=( dim_data["data_original"].dt.day).astype(int),
+        semestre= np.where(dim_data["data_original"].dt.quarter>2,2,1),
+        dia_da_semana= (dim_data["data_original"].dt.day_of_week).astype(int)
     )
+    dim_data["sk_data"] = df_limpo["data"].dt.strftime('%Y%m%d').astype(int)
 
-
-
+    dim_data = dim_data.drop(columns=["data_original"])
     
-    dim_data["data"]= dim_data["data"].astype("str")
-    dim_data = dim_data.rename(columns={"data":"sk_data"})
 
     return dim_data
 
@@ -47,10 +50,18 @@ async def busca_data_completa(client, id_imdb):
                 dados = resp.json()
                 results = dados.get("movie_results")
                 if results:
+                    lista_ids_datas[id_imdb]= results[0].get("release_date")
                     return {"data": results[0].get("release_date")}
             return None
         except Exception:
             return None
+def exclui_tabelas_aux(*tabelas,db):
+
+    with db.connect() as conexao:
+        for tabela in tabelas:
+                conexao.execute(text(f"DROP TABLE IF EXISTS {tabela}"))
+                conexao.commit()
+                print(f"Tabela {tabela} excluída!")
 
 async def main():
     # Converta para lista para garantir estabilidade na iteração
@@ -81,13 +92,34 @@ async def main():
         df_resultados = pd.DataFrame(final_results)
         print(f"\nSucesso! {len(df_resultados)} filmes encontrados.")
         dim_data= transforma_dim_data(df_resultados)
-        dim_data["sk_data"] = dim_data["sk_data"].str.replace("-","").astype("int")
+        
+        dim_data = dim_data.drop_duplicates()
+        
         dim_data.to_sql('dim_data', db, if_exists='delete_rows', index=False)
+
+
+        df_aux =pd.DataFrame(lista_ids_datas.items(),columns=(["id_imdb","sk_data"]))
+
+        df_aux['sk_data'] = pd.to_datetime(df_aux['sk_data'], errors='coerce')
+        df_aux = df_aux.dropna(subset=['sk_data'])
+        df_aux['sk_data'] = df_aux['sk_data'].dt.strftime('%Y%m%d').astype(int)
+
+        df_aux.to_sql("tabela_aux_datas_ids",
+                      con=db,
+                      if_exists="replace",
+                      index=False)
+        query = "select dim_filme.sk_filme, tabelaId.sk_data, tabela_aux.nota_media, tabela_aux.numero_votos, tabela_aux.tempo_minutos from tabela_aux INNER join dim_filme on tabela_aux.id_imdb = dim_filme.id_imdb INNER JOIN tabela_aux_datas_ids tabelaId on tabela_aux.id_imdb = tabelaId.id_imdb"
+        df_fato_filme = pd.read_sql(query,db)
+        df_fato_filme.to_sql("fato_filme",
+                             con=db,
+                             if_exists="replace",
+                             index=False)
+        exclui_tabelas_aux("tabela_aux_datas_ids","tabela_aux",db=db)
 
     else:
         print("\nNenhum dado foi coletado.")
 
 if __name__ == "__main__":
     # Garanta que a leitura do SQL está aqui ou carregada globalmente
-    dim_filme = pd.read_sql("SELECT id_imdb FROM dim_filme LIMIT 10", db)
+    dim_filme = pd.read_sql("SELECT id_imdb FROM dim_filme", db)
     asyncio.run(main())
